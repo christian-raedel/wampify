be7befc64db3e22e1260f899a6e52e302c3fbefe
Christian Rädel <christian.raedel@gmail.com>
2014-08-21 13:04:13 +0200

    Add pub/sub function.

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,8 +1,12 @@
 var _ = require('lodash')
-    , expect = require('chai').expect
+    , chai = require('chai')
+    , expect = chai.expect
+    , spies = require('chai-spies')
     , path = require('path')
     , WebSocket = require('ws')
     , Server = require('../lib/server');
+
+chai.use(spies);
 
 describe('WebsocketServer', function() {
     it('should instanciates', function(done) {
@@ -56,7 +60,7 @@ describe('WebSocketServer:RPC', function() {
         server.loadPlugins();
 
         var ws = new WebSocket('ws://localhost:3000')
-            , message = ['RPC', 'test:echo', 4327, 'hello $inge!'];
+            , message = ['RPC', 'test:echo', 4327, {}, 'hello $inge!'];
         ws.on('open', function() {
             ws.send(JSON.stringify(message));
         });
@@ -67,7 +71,107 @@ describe('WebSocketServer:RPC', function() {
                 done(err);
             }
             expect(data).to.be.deep.equal(message);
+            ws.close();
             done();
         });
+    });
+});
+
+describe('WebSocketServer:PUB/SUB', function() {
+    this.timeout(3000);
+
+    var server = null;
+
+    before(function() {
+        server = new Server({port: 3000});
+    });
+
+    after(function(done) {
+        server.close();
+        setTimeout(function() {
+            done();
+        }, 2000);
+    });
+
+    it('should add a new channel', function() {
+        server = server.addChannel('test:echo', {ack: true});
+        expect(server).to.be.an.instanceof(Server);
+        expect(server.channels).to.be.deep.equal({
+            'test:echo': {
+                sockets: [],
+                opts: {
+                    ack: true
+                }
+            }
+        });
+    });
+
+    it('should remove an existing channel', function() {
+        server = server.removeChannel('test:echo');
+        expect(server).to.be.an.instanceof(Server);
+        expect(server.channels).to.be.deep.equal({});
+    });
+
+    it('should subscribe to an existing channel', function(done) {
+        server.addChannel('test:$inge', {ack: true});
+
+        var ws = new WebSocket('ws://localhost:3000')
+            , message = ['SUB', 'test:$inge', 2743, {ack: true}];
+        ws.on('open', function() {
+            ws.send(JSON.stringify(message));
+        });
+        ws.on('message', function(data) {
+            try {
+                data = JSON.parse(data);
+            } catch(err) {
+                done(err);
+            }
+            expect(data).to.be.deep.equal(['ACK', 'test:$inge', 2743, {}, true]);
+            ws.close();
+            done();
+        });
+    });
+
+    it('should publish to an existing channel', function(done) {
+        expect(server.channels['test:$inge']).to.be.ok;
+
+        var ws = new WebSocket('ws://localhost:3000');
+
+        var spyB = null;
+        function onopen() {
+            function onmessage(data) {
+                try {
+                    data = JSON.parse(data);
+                } catch (err) {
+                    done(err);
+                }
+
+                console.log(data);
+                switch (data[0]) {
+                    case 'PUB':
+                        expect(data).to.be.deep.equal(['PUB', 'test:$inge', 2743, {}, 'hello $inge']);
+                        break;
+                    case 'ACK':
+                        expect(data).to.be.deep.equal(['ACK', 'test:$inge', 2743, {}, true]);
+                        break;
+                    default:
+                        done(new Error('Invalid message response!'));
+                }
+            }
+            spyB = chai.spy(onmessage);
+            ws.on('message', spyB);
+
+            ws.send(JSON.stringify(['PUB', 'test:$inge', 2743, {}, 'hello $inge']));
+        }
+
+        var spyA = chai.spy(onopen);
+        ws.on('open', spyA);
+
+        setTimeout(function() {
+            expect(spyA).to.have.been.called.once;
+            expect(spyB).to.have.been.called.twice;
+            ws.close();
+            done();
+        }, 2000);
     });
 });
